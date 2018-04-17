ded85c0c30ca5297c43e01d3069dc7deb0487d6c
Vishaal <vishaal16119@iiitd.ac.in>
2018-04-18 04:19:33 +0530

    Completed dispayEquipments and Trainers

--- app/src/main/java/com/example/android/gymapp/DataActivity.java
+++ app/src/main/java/com/example/android/gymapp/DataActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.android.gymapp.Databases.DisplayCustomerActivity;
+import com.example.android.gymapp.DisplayCustomerActivity;
 
 import butterknife.ButterKnife;
 
